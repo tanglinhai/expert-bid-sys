46ecc5dc8909921331078098dee80ed5fa441734
xzrll915 <luling@365trade.com.cn>
2019-03-13 12:08:35 +0800

    shoyush sho首页

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -3,77 +3,70 @@
         <!--首页-->
         <el-row>
             <el-col :span="24">
-                <div class="tips">
-                    <p>欢迎您，<br><span class="fs16">成功登陆中招联合电子招标平台！</span></p>
+                <div class="tips ">
+                    <p>欢迎您，<br><span class="fs24">成功登陆中招联合电子招标平台！</span></p>
                 </div>
             </el-col>
         </el-row>
         <el-row>
-            <el-col :offset="1">
+            <el-col  class="mb60">
                 <el-row class="serve ">
-                    <el-col :span="10">
+                    <el-col :span="11">
                         <div class="grid-content bg-purple">
-                            <i class=" icon iconfont icon-1  col606266"
-                               style="margin-right: 20px;font-size: 32px;vertical-align: middle;"></i><span
-                                class="col606266">客服支持：400-092-8199</span>
+                            <i class=" icon iconfont icon-1  col606266 mr20 ver_al_m" ></i><span
+                                class="col606266 fs19 ver_al_m">客服支持：400-092-8199</span>
                         </div>
                     </el-col>
-                    <el-col :span="10">
+                    <el-col :span="11">
                         <div class="grid-content bg-purple">
-                            <i class=" icon iconfont icon-2 col606266 "
-                               style="margin-right: 20px;font-size: 32px;vertical-align: middle;"></i><span
-                                class="col606266">客服支持：400-092-8199</span>
+                            <i class=" icon iconfont icon-2 col606266  ver_al_m mr20"></i><span
+                                class="col606266 fs19 ver_al_m">客服邮箱：zzlh@365trade.com.cn</span>
                         </div>
                     </el-col>
                 </el-row>
                 <el-row >
-                    <el-col :span="7" style="float: left">
+                    <el-col :span="10"  class="fl">
                             <el-row>
                                 <el-col>
-                                    <div class="fwb" style="font-size: 17px;margin-bottom: 30px;">
+                                    <div class="fwb fs20 mb50" >
                                         稳定可靠的电子交易平台专业化顾问式的资讯平台
                                     </div>
                                 </el-col>
                             </el-row>
-                            <el-row class="col606266" style="margin-bottom: 30px;">
+                            <el-row class="col606266 mb40 " style="  line-height:2em;">
                                 <el-col>
-                                    <div class="fwb" style="font-size: 17px;
-        line-height: 1.5em;
-    ">效率：符合互联网行业“快、大、好”的特点。
+                                    <div class="fwb fs18" >效率：符合互联网行业“快、大、好”的特点。
                                     </div>
                                 </el-col>
                                 <el-col>
-                                    <div class="fwb" style="font-size: 17px;
-        line-height: 1.5em;
-    ">规则：体制内外的规则均要无条件遵守。
+                                    <div class="fwb fs18" >规则：体制内外的规则均要无条件遵守。
                                     </div>
                                 </el-col>
                                 <el-col>
-                                    <div class="fwb" style="font-size: 17px; line-height: 1.5em;
-    ">责任：从高层到每一个员工，任务贯穿工作始终。
+                                    <div class="fwb fs18" >责任：从高层到每一个员工，任务贯穿工作始终。
                                     </div>
                                 </el-col>
                             </el-row>
-                        <el-row>
-                            <el-col style=" line-height: 1.8em;">
-                                <i class=" icon iconfont icon-duochucunzhichi  col606266"
-                                   style="margin-right: 20px;vertical-align: middle;"></i> <span style="margin-right: 20px">工信支持</span><span  style="margin-right: 20px">强强联合</span>
+                        <el-row style=" line-height: 2.3em;">
+                            <el-col >
+                                <i class=" icon iconfont icon-duochucunzhichi  col606266 mr20 ver_al_m"
+                                  ></i> <span class="mr20 col606266 ">工信支持</span><span class="mr20 col606266 ">强强联合</span>
                             </el-col>
-                            <el-col style=" line-height: 1.8em;">
-                                <i class=" icon iconfont icon-scale1  col606266"
-                                   style="margin-right: 20px;vertical-align: middle;"></i> <span  style="margin-right: 20px">诚信透明</span><span  style="margin-right: 20px">公正权威</span>
+                            <el-col >
+                                <i class=" icon iconfont icon-scale1  col606266 mr20 ver_al_m"
+                                   ></i> <span  class="mr20 col606266 ">诚信透明</span><span  class="mr20 col606266 ">公正权威</span>
                             </el-col>
-                            <el-col style=" line-height: 1.8em;">
-                                <i class=" icon iconfont icon-ziyuan  col606266"
-                                   style="margin-right: 20px;vertical-align: middle;"></i><span  style="margin-right: 20px">资源众多</span><span  style="margin-right: 20px">客户至上</span>
+                            <el-col >
+                                <i class=" icon iconfont icon-ziyuan  col606266 mr20 ver_al_m"
+                                   ></i><span  class="mr20 col606266 ">资源众多</span><span  class="mr20 col606266 ">客户至上</span>
                             </el-col>
-                            <el-col style=" line-height: 1.8em;">
-                                <i class=" icon iconfont icon-ronghepindao  col606266"
-                                   style="margin-right: 20px;vertical-align: middle;"></i><span  style="margin-right: 20px">跨界融合</span><span  style="margin-right: 20px">专注极致</span>
+                            <el-col >
+                                <i class=" icon iconfont icon-ronghepindao  col606266  mr20 ver_al_m"
+                                  ></i><span  class="mr20 col606266 ">跨界融合</span><span  class="mr20 col606266 ">专注极致</span>
                             </el-col>
                         </el-row>
                     </el-col>
-                    <el-col :span="17" style="float:right">
+                    <el-col :span="13" class="fr" style="overflow: hidden">
                         <div >
                             <img src="../assets/img/index_icon.jpg" alt="" >
                         </div>
@@ -100,17 +93,16 @@
             border-radius: 4px;
             border-left: 5px solid #50bfff;
             p {
-                font-size: 18px;
+                font-size: 28px;
                 color: #5e6d82;
                 line-height: 1.5em;
             }
         }
         .serve {
-            padding: 40px 0 40px 0;
-            .ml21 {
-
-            }
+            padding: 60px 0 60px 0;
+               i{
+                   font-size: 34px;
+               }
         }
-
     }
 </style>
--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -1,17 +1,71 @@
 <template>
     <div class="projects">
-        项目列表页面
+        <!-- 项目列表页面-->
+        <el-row class="mb15">
+            <el-col>
+                <el-button type="primary" size="medium">专家评审/ 我的评审项目</el-button>
+            </el-col>
+        </el-row>
+        <div class="main">
+            <el-row class="pro_msg_warp ">
+                <div class="grid-content bg-purple-dark fl pro_msg_div textAlignL">
+                    <h5 class="commonTitle col348fe2">项目信息</h5>
+                </div>
+                <div class="fl search_warp textAlignR" >
+                    <el-input
+                            placeholder="项目名称关键字进行检索"
+                            v-model="seach_input"
+                            clearable size="small" class="wi260 ">
+                    </el-input>
+                    <el-button type="primary"
+                               size="small"
+                               class=" search_btn"
+                               :loading="btn_search_loading"
+                               @click="search()"
+                               icon="el-icon-search">查询</el-button>
+                </div>
+            </el-row>
+        </div>
     </div>
 </template>
-
 <script>
-export default {
-  name: 'projects',
-  props: {}
-}
+
+    export default {
+        name: 'projects',
+        props: {},
+        components: {},
+        data() {
+            return {
+                seach_input:""
+            }
+        },
+        mounted() {
+
+        },
+        methods: {},
+    }
 </script>
 <style lang="scss">
-.index {
-    
-}
+    .projects {
+        background-color: #ededed;
+        padding: 15px 20px 15px 20px;
+        .main {
+            background: white;
+            border-radius: 5px;
+            .pro_msg_warp {
+                padding: 20px 15px 20px 15px;
+                .pro_msg_div,.search_warp{
+                    width: 50%;
+                }
+                .search_warp{
+                    .el-button--small{
+                        border-radius: 0 3px 3px 0;
+                    }
+                    .el-input__inner{
+                        border-radius:4px 0 0 4px;
+                    }
+                }
+            }
+        }
+    }
 </style>
