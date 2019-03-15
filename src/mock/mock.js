346034cef4730439911fdbd54f53470bf89fa0cb
xzrll915 <luling@365trade.com.cn>
2019-03-15 16:23:53 +0800

    招标文件弹框页面上上数据模拟

--- src/mock/mock.js
+++ src/mock/mock.js
@@ -1,7 +1,7 @@
 ﻿import Mock from 'mockjs';
 Mock.setup({
     timeout: 1000
-})
+});
 
 const Random = Mock.Random;
 let tableMs = Mock.mock('/api/tableMsg', 'post', {
@@ -11,7 +11,7 @@ let tableMs = Mock.mock('/api/tableMsg', 'post', {
         date: () => Random.date(),
         'readM': '审核通过'
     }]
-})
+});
 //评审项目数据列表
 let bagTitMs = Mock.mock('/api/bagMsg', 'post', {
     'projectBagMsg|1-5': [{
@@ -23,15 +23,33 @@ let bagTitMs = Mock.mock('/api/bagMsg', 'post', {
     }],
     'allBagMsg|1-3': [{
         id: () => Random.id(),
-        'groupName': '0' + Random.natural(0, 9)+ Random.natural(0, 0) + Random.natural(0, 9)+'-'
-        +Random.natural(0, 6) + Random.natural(5, 9)+ Random.natural(0, 6) + Random.natural(5, 9)+'N'+Random.natural(0, 6) + Random.natural(5, 9)+ Random.natural(0, 6)
+        'groupName': '0' + '6'+ Random.natural(0, 0) + Random.natural(0, 9)+'-'
+        +Random.natural(0, 6) + Random.natural(5, 9)+ Random.natural(0, 6) + Random.natural(5, 9)+'N'
+        +Random.natural(0, 6) + Random.natural(5, 9)+ Random.natural(0, 6)
         +'/'+'评委会',
         'bagName': '第' + Random.natural(0, 6) + Random.natural(5, 9)+'包',
         'starTime':   () => Random.datetime(),
         'stopTime':  () => Random.datetime(),
-        'type|1': [0, 1]
+        'type|1': [0, 1],
+        'proBusinessNum':'0' + '6'+ Random.natural(0, 0) + Random.natural(0, 9)+'-'
+        +Random.natural(0, 6) + Random.natural(5, 9)+ Random.natural(0, 6) + Random.natural(5, 9)+'q'+'w'+'e'+'r'+'N'
+        +Random.natural(0, 9) + Random.natural(0, 9)+ Random.natural(0, 9)+ Random.natural(0, 9),
+        'sellStartTime':() => Random.datetime(),
+        'sellEndTime':() => Random.datetime(),
+        'bidOpeningTime':() => Random.datetime(),
+        'getBidType':'在线下载',
+        'bidOpeningPlace': '北京海淀区',
+        'evaluationBidPlace': '北京市朝阳区',
+        'evaluationBidTime':() => Random.datetime(),
+        'paymentType':[0,1,2],
+        'uploadWay|1':[0,1,2],
+        'tenderFree':Random.float(10, 1000, 2, 2),
+        'postage':Random.float(10, 100, 2, 2),
+        'drawingDeposit':Random.float(10, 100, 2, 2),
+        'bidAgencyServiceFee':Random.float(1, 100, 2, 2)+'(浮动费率 （发改价格[2011]534号)',
+        'tenderBond':Random.float(10, 100, 2, 2)+'万元'
     }],
-})
+});
 
 
 
