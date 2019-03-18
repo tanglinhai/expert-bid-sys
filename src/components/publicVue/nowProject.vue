66d7de6bf9d29d695a57a59ba32170e76fdc829b
xzrll915 <luling@365trade.com.cn>
2019-03-18 10:22:31 +0800

    tan弹框单选复选渲染

--- src/components/publicVue/nowProject.vue
+++ src/components/publicVue/nowProject.vue
@@ -47,11 +47,9 @@
                         </el-col>
                         <el-col style="width:30%" class="btns_div">
                             <el-col :span="11" class="begin_bidding" style="text-align: right">
-
-                                    <el-button size="small" class="btnBg" @click="begin_pingbiao_btn"><i
-                                            class="icon iconfont icon-kaishi mr3"></i>开始评标
-                                    </el-button>
-
+                                <el-button size="small" class="btnBg" @click="begin_pingbiao_btn"><i
+                                        class="icon iconfont icon-kaishi mr3"></i>开始评标
+                                </el-button>
                             </el-col>
                             <el-col :span="11" class="bidding_doc" style="margin-left: 15px">
                                 <el-button size="small" @click="bidding_doc_btn(item)"><i
@@ -83,13 +81,13 @@
                         <div class="overflowText textAlignR">项目业务编号：</div>
                     </el-col>
                     <el-col :span="6">
-                        <div class="overflowText">{{msgBox.proBusinessNum}}</div>
+                        <div class="overflowText">{{every_msg.proBusinessNum}}</div>
                     </el-col>
                     <el-col :span="6">
                         <div class="overflowText textAlignR">开标地点：</div>
                     </el-col>
                     <el-col :span="6">
-                        <div class="overflowText">{{msgBox.bidOpeningPlace}}</div>
+                        <div class="overflowText">{{every_msg.bidOpeningPlace}}</div>
                     </el-col>
                 </el-row>
                 <el-row class="information">
@@ -97,13 +95,13 @@
                         <div class="overflowText textAlignR">售标起始时间：</div>
                     </el-col>
                     <el-col :span="6">
-                        <div class="overflowText">{{msgBox.sellStartTime}}</div>
+                        <div class="overflowText">{{every_msg.sellStartTime}}</div>
                     </el-col>
                     <el-col :span="6">
                         <div class="overflowText textAlignR">开标时间：</div>
                     </el-col>
                     <el-col :span="6">
-                        <div class="overflowText">{{msgBox.bidOpeningTime}}</div>
+                        <div class="overflowText">{{every_msg.bidOpeningTime}}</div>
                     </el-col>
                 </el-row>
                 <el-row class="information">
@@ -111,13 +109,13 @@
                         <div class="overflowText textAlignR">售标结束时间：</div>
                     </el-col>
                     <el-col :span="6">
-                        <div class="overflowText">{{msgBox.sellEndTime}}</div>
+                        <div class="overflowText">{{every_msg.sellEndTime}}</div>
                     </el-col>
                     <el-col :span="6">
                         <div class="overflowText textAlignR">评标地点：</div>
                     </el-col>
                     <el-col :span="6">
-                        <div class="overflowText">{{msgBox.evaluationBidPlace}}</div>
+                        <div class="overflowText">{{every_msg.evaluationBidPlace}}</div>
                     </el-col>
                 </el-row>
                 <el-row class="information">
@@ -125,16 +123,15 @@
                         <div class="overflowText textAlignR">获取标书方式：</div>
                     </el-col>
                     <el-col :span="6">
-                        <div class="overflowText">{{msgBox.getBidType}}</div>
+                        <div class="overflowText">{{every_msg.getBidType}}</div>
                     </el-col>
 
                     <el-col :span="6">
                         <div class="overflowText textAlignR">评标时间：</div>
                     </el-col>
                     <el-col :span="6">
-                        <div class="overflowText">{{msgBox.evaluationBidTime}}</div>
+                        <div class="overflowText">{{every_msg.evaluationBidTime}}</div>
                     </el-col>
-
                 </el-row>
                 <el-row class="information">
                     <el-col :span="6">
@@ -142,23 +139,19 @@
                     </el-col>
                     <el-col :span="6">
                         <div class="overflowText">
-                            <template>
-                                <el-checkbox v-model="checked1" disabled >网上支付</el-checkbox>
-                                <el-checkbox v-model="checked2" disabled >现金支付</el-checkbox>
-                                <el-checkbox v-model="checked3" disabled >电汇</el-checkbox>
-                            </template>
-                        </div>
+                            <el-checkbox v-model="checked1" disabled class="checkedS">网上支付</el-checkbox>
+                            <el-checkbox v-model="checked2" disabled class="checkedS">现金支付</el-checkbox>
+                            <el-checkbox v-model="checked3" disabled class="checkedS">电汇</el-checkbox>
+                         </div>
                     </el-col>
                     <el-col :span="6">
                         <div class="overflowText textAlignR">上传方式：</div>
                     </el-col>
                     <el-col :span="6">
                         <div class="overflowText">
-
-                                <el-radio disabled v-model="radio1" label="禁用" class="radioS">线上</el-radio>
-                                <el-radio disabled v-model="radio1" label="禁用2" class="radioS">线下</el-radio>
-                                <el-radio disabled v-model="radio1" label="禁用3" class="radioS">辅助开评标</el-radio>
-
+                            <el-radio disabled v-model="radio1" label="禁用" class="radioS">线上</el-radio>
+                            <el-radio disabled v-model="radio1" label="禁用2" class="radioS">线下</el-radio>
+                            <el-radio disabled v-model="radio1" label="禁用3" class="radioS">辅助开评标</el-radio>
                         </div>
                     </el-col>
                 </el-row>
@@ -176,13 +169,13 @@
                         <div class="overflowText textAlignR">包名称：</div>
                     </el-col>
                     <el-col :span="6">
-                        <div class="overflowText">{{msgBox.bagName}}</div>
+                        <div class="overflowText">{{every_msg.bagName}}</div>
                     </el-col>
                     <el-col :span="6">
                         <div class="overflowText textAlignR">图纸押金：</div>
                     </el-col>
                     <el-col :span="6">
-                        <div class="overflowText">{{msgBox.drawingDeposit}}元</div>
+                        <div class="overflowText">{{every_msg.drawingDeposit}}元</div>
                     </el-col>
                 </el-row>
                 <el-row class="information">
@@ -190,14 +183,14 @@
                         <div class="overflowText textAlignR">邮寄费：</div>
                     </el-col>
                     <el-col :span="6">
-                        <div class="overflowText">{{msgBox.postage}}元</div>
+                        <div class="overflowText">{{every_msg.postage}}元</div>
                     </el-col>
 
                     <el-col :span="6">
                         <div class="overflowText textAlignR">投标保证金：</div>
                     </el-col>
                     <el-col :span="6">
-                        <div class="overflowText">{{msgBox.tenderBond}}元</div>
+                        <div class="overflowText">{{every_msg.tenderBond}}元</div>
                     </el-col>
                 </el-row>
                 <el-row class="information">
@@ -205,14 +198,14 @@
                         <div class="overflowText textAlignR">标书费：</div>
                     </el-col>
                     <el-col :span="6">
-                        <div class="overflowText">{{msgBox.tenderFree}}元</div>
+                        <div class="overflowText">{{every_msg.tenderFree}}元</div>
                     </el-col>
 
                     <el-col :span="6">
                         <div class="overflowText textAlignR">招标代理服务费：</div>
                     </el-col>
                     <el-col :span="6">
-                        <div class="overflowText">{{msgBox.bidAgencyServiceFee}}</div>
+                        <div class="overflowText">{{every_msg.bidAgencyServiceFee}}</div>
                     </el-col>
                 </el-row>
             </div>
@@ -279,12 +272,11 @@
         data() {
             return {
                 docListDialogVisible: false,//文件列表弹框
-                checked1: true,
-                checked2: true,
-                checked3: true,
-                radio1: '禁用2',
-                msgBox:[]
-
+                checked1: '',
+                checked2: '',
+                checked3: '',
+                radio1: '',
+                every_msg:[],
             }
         },
         // 父组件传过来的值
@@ -306,15 +298,23 @@
 
         },
         methods: {
-            bidding_doc_btn(obj) {
+            bidding_doc_btn(obj) {//招标相关文件
                 this.docListDialogVisible = true;
-                console.log(obj.paymentType,obj.uploadWay);
-                this.msgBox=obj;
+                if(obj.paymentType[0]==0){
+                    this.checked1=true;
+                }if(obj.paymentType[1]==1){
+                    this.checked2=true;
+                }
+                if(obj.paymentType[2]==2){
+                    this.checked3=true;
+                }
+                this.every_msg=obj;
+                setTimeout(() =>{
+                    this.radio1=$(".radioS")[obj.uploadWay].children[0].children[1].value;
 
-
-
+                },100)
             },
-            begin_pingbiao_btn() {
+            begin_pingbiao_btn() {//开始评标
 
             }
         }
