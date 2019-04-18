f5129c946b1fbdb4b9881aafc6a2fb6558d422bc
tanglinhai <tanglinhai@365trade.com.cn>
2019-04-18 15:07:19 +0800

    ''

--- src/views/qualificationExam/StartEvaluation.vue
+++ src/views/qualificationExam/StartEvaluation.vue
@@ -986,14 +986,14 @@
                                 .pdfobject {
                                     display: block;
                                 }
-                                .floating_div {
-                                    position: absolute;
-                                    top: 0;
-                                    left: 0;
-                                    width: 100%;
-                                    height: 100%;
-                                    z-index: 1;
-                                }
+                            }
+                            .floating_div {
+                                position: absolute;
+                                top: 0;
+                                left: 0;
+                                width: 100%;
+                                height: 100%;
+                                z-index: 1;
                             }
                         }
                     }
