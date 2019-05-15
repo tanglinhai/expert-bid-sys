1e86b9a6a34b58c5a52846a1e502f6f9b1490dbb
lipan <lipan365trade.com.cn>
2019-05-15 09:23:25 +0800

    mock修改

--- src/mock/lp.js
+++ src/mock/lp.js
@@ -399,7 +399,8 @@ function get_data(currentPage,is_submit_type,types) {//type
         }
         if (currentPage == 2) {//资格审查
             if(types==1){
-                var num=[5,4, 4, 4, 4, 4, 4, 4,4, 4, 4, 4, 4,4];
+                var num=[1,4, 4, 4, 4, 4, 4, 4,4, 4, 4, 4, 4,4];
+                num.splice(types-1,1,5)
                 num.splice(currentPage-1,1,is_submit_type?1:3);
                 return num;
             }else{
@@ -408,11 +409,13 @@ function get_data(currentPage,is_submit_type,types) {//type
         }
         if (currentPage == 3) {//资格审查汇总
             if(types==1){
-                var num=[1,5,4, 4, 4, 4, 4, 4,4, 4, 4, 4, 4,4];
+                var num=[1,1,4, 4, 4, 4, 4, 4,4, 4, 4, 4, 4,4];
+                num.splice(types-1,1,5);
                 num.splice(currentPage-1,1,is_submit_type?1:3);
                 return num;
             }else if(types==2){
-                var num=[1,5,4, 4, 4, 4, 4, 4,4, 4, 4, 4, 4,4];
+                var num=[1,1,4, 4, 4, 4, 4, 4,4, 4, 4, 4, 4,4];
+                num.splice(types-1,1,5);
                 num.splice(currentPage-1,1,is_submit_type?1:3);
                 return num;
             }else{
